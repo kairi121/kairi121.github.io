cff9e65717672fdfd5e6d48c5a965404899f2e3d
kairi121 <takakai121@gmail.com>
2024-06-20 15:13:55 +0900

    fix profile

--- my-website/src/components/Profile.js
+++ my-website/src/components/Profile.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../HomePage.css';
 
 const Profile = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 576);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 576);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <Container className="text-center mt-5">
-      <Row className="justify-content-center">
+    <Container style={{ textAlign: 'center', marginTop: '1rem' }} >
+      <Row style={{ display: 'flex', justifyContent: 'center' }}>
         <Col xs={12} md={6} lg={4}>
           <div className="circle-container">
             <img
@@ -16,10 +26,10 @@ const Profile = () => {
           </div>
         </Col>
       </Row>
-      <Row className="justify-content-center mt-3">
+      <Row style={{ marginTop: '1rem' }}>
         <Col xs={12}>
-          <h4>KAIRI TAKAKI</h4>
-          <h6>@Nagoya University, Japan</h6>
+          <h4 style={{ fontSize: isSmallScreen ? '1.5rem' : '2rem' }}>KAIRI TAKAKI</h4>
+          <h6 style={{ fontSize: isSmallScreen ? '1.2rem' : '1.5rem' }}>@Nagoya University, Japan</h6>
         </Col>
       </Row>
     </Container>
@@ -27,3 +37,4 @@ const Profile = () => {
 };
 
 export default Profile;
+
